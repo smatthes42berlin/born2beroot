30bfd63af6a0823347eb25bd629105cf9a116884
Steffen Matthes <smatthes@c4c4c6.42berlin.de>
2023-08-04 18:37:08 +0200

    ab gehts

--- sys_info.sh
+++ sys_info.sh
@@ -6,7 +6,7 @@
 #    By: smatthes <smatthes@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/29 08:01:03 by smatthes          #+#    #+#              #
-#    Updated: 2023/08/02 15:54:42 by smatthes         ###   ########.fr        #
+#    Updated: 2023/08/04 18:30:20 by smatthes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,16 +21,14 @@ cpu=$(lscpu | awk '/^CPU\(s\):/ {print $2}')
 
 vcpu=$(nproc)
 
-totalmem=$(free | awk '/Mem:/ {print $2}')
-totalmemmb=$(echo "scale=1; $totalmem / 1000" | bc)
-usedmem=$(free | awk '/Mem:/ {print $3}')
-usedmemmb=$(echo "scale=1; $usedmem / 1000" | bc)
-mempercent=$(echo "scale=2; $usedmem / $totalmem * 100" | bc)
+totalmemmb=$(free -k | awk '/Mem:/ {printf("%.0f"), $2/1024}')
+usedmemmb=$(free -k | awk '/Mem:/ {printf("%.0f"), $3/1024}')
+mempercent=$(awk "BEGIN { printf \"%.2f\", 100 * $usedmemmb / $totalmemmb }")
 
-diskspaceusedmb=$(df -BM | awk 'BEGIN{NR > 1; sum=0} {sum=sum+$3} END{print sum}')
-diskspacetotalmb=$(df -BM | awk 'BEGIN{NR > 1; sum=0} {sum=sum+$2} END{print sum}')
-diskspacetotalgb=$(df -BG | awk 'BEGIN{NR > 1; sum=0} {sum=sum+$2} END{print sum}')
-diskspacepercent=$(echo "scale=0; $diskspaceusedmb * 100 / $diskspacetotalmb" | bc)
+diskspaceusedmb=$(df --total -BM | awk 'END {print substr($3, 1, length($3)-1)}')
+diskspacetotalmb=$(df --total -BM | awk 'END {print substr($2, 1, length($3)-1)}')
+diskspacetotalgb=$(df --total -BG | awk 'END {print substr($2, 1, length($3)-1)}')
+disksapcepercent=$(awk "BEGIN { printf \"%.0f\", 100 * $diskspaceusedmb / $diskspacetotalmb }")
 
 cpuusage=$(top -n1 | awk 'BEGIN {sum=0} NR>7 {sum=sum+$10} END{print sum}')
 
@@ -39,23 +37,22 @@ lastboot=$(who -b | awk '{print $3" "$4}')
 lvmvolumes=$(lsblk | awk '/lvm/ {print $0}' | wc -l)
 lvmuse=$(if [ $lvmvolumes -eq 0 ]; then echo no; else echo yes; fi)
 
-# 2>/dev/null means all errors are justed thrown way/ignored
-numbertcpestab=$(netstat -t 2>/dev/null | awk '/ESTABLISHED/ {print $0}' | wc -l)
+numbertcpestab=$(ss | awk '/^tcp/ {print $0}' | awk '/ESTAB/ {print $0}' | wc -l)
 
-numberofussers=$(who | wc -l)
+numberofussers=$(users | wc -l)
 
 ipaddr=$(hostname -I | awk '{print $1}')
 macaddr=$(ip a | awk '/link\/ether/ {print $2; exit}')
 
-numcmdssudo=$(awk '{print $0}' /var/log/auth.log | wc -l)
+numcmdssudo=$(awk '{print $0}' /var/log/sudo/sudo.log | wc -l)
 
-wall "  Broadcast message from $username@$hostname ${tty} ($date):
-            #Architecture: $architecture
+
+wall "      #Architecture: $architecture
             #CPU physical : $cpu
             #vCPU : $vcpu
-            #Memory Usage : $usedmemmb/$totalmemmbMb ($mempercent%)
-            #Disk Usage : $diskspaceusedmb/$diskspacetotalgb ($diskspacepercent%)
-            #CPU load : $cpuusage%
+            #Memory Usage : ${usedmemmb}/${totalmemmb}Mb (${mempercent}%)
+            #Disk Usage : ${diskspaceusedmb}/${diskspacetotalgb}Gb (${disksapcepercent}%)
+            #CPU load : ${cpuusage}%
             #Last boot : $lastboot
             #LVM use : $lvmuse
             #Connections TCP : $numbertcpestab ESTABLISHED
